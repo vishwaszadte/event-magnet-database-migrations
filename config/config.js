a090172df371abcac66e9c9661116970bcfc4e03
Vishwas <vishwaszadte@gmail.com>
2023-10-02 14:20:08 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,12 +9,6 @@ dbConfig = {
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Set to false if your database doesn't use a valid SSL certificate
-      },
-    },
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -24,12 +18,6 @@ dbConfig = {
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Set to false if your database doesn't use a valid SSL certificate
-      },
-    },
   },
   production: {
     username: process.env.DB_USERNAME,
